53a97a8cd69fa061fe74a3a3aceb0553c6d1dc94
MukbanianiZura <zuramukbaniani743@gmail.com>
2024-11-18 21:10:53 +0400

    Update math_operations.py

--- math_operations.py
+++ math_operations.py
@@ -1,14 +1,30 @@
 def addition(x, y):
+    """
+        x: int
+        y: int
+    """
     return x + y
 
 
 def subtraction(x, y):
+    """
+        x: int
+        y: int
+    """
     return x - y
 
 
 def multiplication(x, y):
+    """
+        x: int
+        y: int
+    """
     return x * y
 
 
 def division(x, y):
+    """
+        x: int
+        y: int
+    """
     return x / y
